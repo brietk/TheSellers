742065725d77423c4022332eba122a1f254fa06c
helga13 <helga13@ru.is>
2017-03-05 18:40:15 +0000

    add

--- client/src/app/sellers/sellers.component.ts
+++ client/src/app/sellers/sellers.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SellersService, Seller, SellerProduct } from '../sellers.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { SellerDlgComponent } from '../seller-dlg/seller-dlg.component';
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-sellers',
@@ -11,14 +12,34 @@ import { SellerDlgComponent } from '../seller-dlg/seller-dlg.component';
 export class SellersComponent implements OnInit {
 
     sellers: Seller[];
+    id: number;
+    error: string;
 
-constructor(private modalService: NgbModal, private service: SellersService){}
+constructor(private modalService: NgbModal, private service: SellersService, 
+  private router: Router,  private route: ActivatedRoute) { }
 
-  ngOnInit() {
-    this.service.getSellers().subscribe(result => {
+refreshList(){
+      this.service.getSellers().subscribe(result => {
       this.sellers = result;
     });
+}
+
+  ngOnInit() {
+   this.id = this.route.snapshot.params['id'];
+    console.log("sellerrid " + this.id);
+    this.refreshList();
+
+    var successHandler = (result)=> { this.sellers = result;}
+    var errorHandler = (err) => { 
+      console.log("something failed with status: " + err.status);
+      if(err.status == 404) {
+        console.log("yes, it faild");
+        this.error = "Sorry seller with id: " + this.id + " does not exist";
+      }
+    }
+    this.service.getSellerById(this.id).subscribe(successHandler, errorHandler);
   }
+  
 
   addSeller(){ 
   const modalInstance = this.modalService.open(SellerDlgComponent);
@@ -33,6 +54,11 @@ constructor(private modalService: NgbModal, private service: SellersService){}
     id: 7*/
   modalInstance.result.then(obj =>{
     console.log("Dialog was closed using OK");
+    this.service.postProduct(this.id, obj.productName, obj.price, obj.quantityInStock, obj.imagePath).subscribe(data => {
+    this.refreshList();
+      }, error => {
+          console.log(error.json());
+      });
     console.log(obj);
   }).catch(err => {
     console.log("Dialog was cancelled");
