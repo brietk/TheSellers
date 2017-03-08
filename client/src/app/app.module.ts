9c179e72006f2ba9a163c1a280a84ac5b3c9bad3
ingaedda <you@example.com>
2017-03-08 00:46:58 +0000

    TOP 10

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Pipe } from '@angular/core';
 import { FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
@@ -10,9 +10,10 @@ import { SellersService } from './sellers.service';
 import { ProductCardComponent } from './product-card/product-card.component';
 import { SellerDlgComponent } from './seller-dlg/seller-dlg.component';
 import { SellersComponent } from './sellers/sellers.component';
-import { ProductsComponent } from './products/products.component';
+import { ProductsComponent, OrderBy } from './products/products.component';
 import { ProductDlgComponent } from './product-dlg/product-dlg.component';
 import { ToastModule } from 'ng2-toastr/ng2-toastr';
+
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { ToastModule } from 'ng2-toastr/ng2-toastr';
     ProductsComponent,
     SellersComponent,
     ProductsComponent,
+    OrderBy,
     ProductDlgComponent
   ],
   imports: [
--- client/src/app/products/products.component.ts
+++ client/src/app/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewContainerRef } from '@angular/core';
+import { Component,Pipe,PipeTransform,  OnInit, Input, ViewContainerRef } from '@angular/core';
 import { SellersService, Seller, SellerProduct } from '../sellers.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { SellerDlgComponent } from '../seller-dlg/seller-dlg.component';
@@ -7,6 +7,74 @@ import { Router, ActivatedRoute } from "@angular/router";
 import { SellersComponent } from '../sellers/sellers.component';
 import { ToastsManager, ToastOptions } from 'ng2-toastr/ng2-toastr';
 
+// https://plnkr.co/edit/DHLVc0?p=info
+@Pipe({name: 'orderBy', pure: false})
+export class OrderBy implements PipeTransform {
+
+    static _orderByComparator(a:any, b:any):number{
+    
+      if((isNaN(parseFloat(a)) || !isFinite(a)) || (isNaN(parseFloat(b)) || !isFinite(b))){
+        //Isn't a number so lowercase the string to properly compare
+        if(a.toLowerCase() < b.toLowerCase()) return 1;
+        if(a.toLowerCase() > b.toLowerCase()) return -1;
+      }
+      else{
+        //Parse strings as numbers to compare properly
+        if(parseFloat(a) < parseFloat(b)) return 1;
+        if(parseFloat(a) > parseFloat(b)) return -1;
+      }
+    
+      return 0; //equal each other
+    }
+
+    transform(input:any, [config = '+']): any{
+        
+        if(!Array.isArray(input)) return input;
+
+        if(!Array.isArray(config) || (Array.isArray(config) && config.length == 1)){
+            var propertyToCheck:string = !Array.isArray(config) ? config : config[0];
+            var desc = propertyToCheck.substr(0, 1) == '-';
+            
+            //Basic array
+            if(!propertyToCheck || propertyToCheck == '-' || propertyToCheck == '+'){
+                return !desc ? input.sort() : input.sort().reverse();
+            }
+            else {
+                var property:string = propertyToCheck.substr(0, 1) == '+' || propertyToCheck.substr(0, 1) == '-'
+                    ? propertyToCheck.substr(1)
+                    : propertyToCheck;
+
+                return input.sort(function(a:any,b:any){
+                    return !desc 
+                        ? OrderBy._orderByComparator(a[property], b[property]) 
+                        : -OrderBy._orderByComparator(a[property], b[property]);
+                });
+            }
+        }
+        else {
+            //Loop over property of the array in order and sort
+            return input.sort(function(a:any,b:any){
+                for(var i:number = 0; i < config.length; i++){
+                    var desc = config[i].substr(0, 1) == '-';
+                    var property = config[i].substr(0, 1) == '+' || config[i].substr(0, 1) == '-'
+                        ? config[i].substr(1)
+                        : config[i];
+
+                    var comparison = !desc 
+                        ? OrderBy._orderByComparator(a[property], b[property]) 
+                        : -OrderBy._orderByComparator(a[property], b[property]);
+                    
+                    //Don't return 0 yet in case of needing to sort by next property
+                    if(comparison != 0) return comparison;
+                }
+
+                return 0; //equal each other
+            });
+        }
+    }
+}
+    
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -14,7 +82,33 @@ import { ToastsManager, ToastOptions } from 'ng2-toastr/ng2-toastr';
   providers: [ ToastsManager ]
 })
 
+/*@Pipe({
+  name: "orderByPipe"
+})
+export class OrderByPipe implements PipeTransform{
+
+ transform(array: Array<string>, args: string): Array<string> {
+
+  if(!array || array === undefined || array.length === 0) return null;
+
+    array.sort((a: any, b: any) => {
+      if (a.number < b.number) {
+        return -1;
+      } else if (a.number > b.number) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    return array;
+  }
+
+}*/
+
+
+
 export class ProductsComponent implements OnInit {
+  
 
 
   products: SellerProduct[];
@@ -73,6 +167,7 @@ export class ProductsComponent implements OnInit {
     }
     this.service.getSellerById(this.id2).subscribe(successHandler, errorHandler);
   }
+  
 
   addProduct() {
 
